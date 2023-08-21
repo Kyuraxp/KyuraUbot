a2e34c6a6ac32dcc613c5115f1d4478e0495e761
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2023-08-21 13:47:24 +0700

    Tes

--- Ubot/modules/bot/inline.py
+++ Ubot/modules/bot/inline.py
@@ -52,7 +52,7 @@ async def get_readable_time(seconds: int) -> str:
     count = 0
     up_time = ""
     time_list = []
-    time_suffix_list = ["s", "m", "jam", "hari"]
+    time_suffix_list = ["s", "m", "h", "d"]
 
     while count < 4:
         count += 1
