21a478dfd899808b9b73977ee26681ab9fa78e52
sᴀᴍɪᴅᴀɪᴛᴇs <112374934+Samidaites2@users.noreply.github.com>
2023-08-25 01:42:57 +0700

    Update data.py

--- Ubot/core/data.py
+++ Ubot/core/data.py
@@ -1,13 +1,11 @@
 from pyrogram.types import InlineKeyboardButton, WebAppInfo
 from Ubot.modules.basic import cmds, CMD_HELP
+
 class Data:
-
-
-   basic = modules.basic
     
-    num_basic_modules = len(basic)
+   num_basic_modules = len(CMD_HELP)
 
-    text_help_menu = (
-        f"**• Help Menu **\n**• Modules: <code>{num_basic_modules} Modules</code>\n**• prefixes **: `None`"
+   text_help_menu = (
+        f"**• Help Menu **\n**• Modules: <code>{num_basic_modules} Modules</code>\n**• prefixes **: `{cmds}`"
     )
-    reopen = [[InlineKeyboardButton("Open", callback_data="reopen")]]
+   reopen = [[InlineKeyboardButton("Open", callback_data="reopen")]]
