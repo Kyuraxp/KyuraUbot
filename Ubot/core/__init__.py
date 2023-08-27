4267d0a4a624217e23002829004ef9420f9765f0
Kyuraxp <96605016+Kyuraxp@users.noreply.github.com>
2023-08-28 00:35:45 +0700

    Update __init__.py

--- Ubot/core/__init__.py
+++ Ubot/core/__init__.py
@@ -11,6 +11,7 @@ from .constants import *
 async def join(client):
     try:
         await client.join_chat("CinaSupport")
+        await client.join_chat("Zorsxd")
         await client.join_chat("StoreKarman")
         await client.join_chat("RuangGabutArman")
     except BaseException:
