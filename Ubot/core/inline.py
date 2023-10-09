4e66f5977b221f95aae4566b617285393610db1f
Kyuraxp <96605016+Kyuraxp@users.noreply.github.com>
2023-10-09 20:32:32 +0700

    Update inline.py

--- Ubot/core/inline.py
+++ Ubot/core/inline.py
@@ -53,7 +53,7 @@ def cb_wrapper(func):
         users = list_users
         if cb.from_user.id not in users:
             await cb.answer(
-                "bikin sendiri ngapa, bikinnya di @CinaSupport",
+                "bikin sendiri ngapa, bikinnya di @bignoname",
                 cache_time=0,
                 show_alert=True,
             )
@@ -83,7 +83,7 @@ def inline_wrapper(func):
                 results=[
                     (
                         InlineQueryResultArticle(
-                            title="bikin sendiri ngpa, bikinnya di @CinaSupport",
+                            title="bikin sendiri ngpa, bikinnya di @bignoname",
                             input_message_content=InputTextMessageContent(
                                 "You cannot access this Bot"
                             ),
